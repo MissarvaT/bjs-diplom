41767488218cab71ac068560d40851615ed36d51
MissarvaT <54108000+MissarvaT@users.noreply.github.com>
2020-03-18 11:49:40 +0300

    arguments reduced

--- public/homePage.js
+++ public/homePage.js
@@ -3,26 +3,26 @@
 const logoutButton = new LogoutButton();
 
 logoutButton.action = () => {
-    return ApiConnector.logout( (err, data) => {
-        if (data) {
+    return ApiConnector.logout( response => {
+        if (response) {
             location.reload();
         };
     });
 };
 
-ApiConnector.current((err, data) => {
-    if (data) {
-        ProfileWidget.showProfile(data);
+ApiConnector.current((response) => {
+    if (response) {
+        ProfileWidget.showProfile(response.data);
     }
 });
 
 const ratesBoard = new RatesBoard();
 
 function updateStocks() {
-    return ApiConnector.getStocks((err, data) => {
-        if (data) {
+    return ApiConnector.getStocks(response => {
+        if (response) {
             ratesBoard.clearTable();
-            ratesBoard.fillTable(data);
+            ratesBoard.fillTable(response.data);
         }})
 };
 
@@ -32,33 +32,33 @@ setInterval(updateStocks, 60000);
 const moneyManager = new MoneyManager();
 
 moneyManager.addMoneyCallback = data => {
-    return ApiConnector.addMoney(data, (err, data) => {
+    return ApiConnector.addMoney(data, response => {
         if (err) {
             moneyManager.setMessage(true, err);
-        } else if (data) {
-            ProfileWidget.showProfile(data);
+        } else if (response) {
+            ProfileWidget.showProfile(response.data);
             moneyManager.setMessage(false, 'Баланс успешно пополнен!')
         }
     })
 };
 
 moneyManager.conversionMoneyCallback = data => {
-    return ApiConnector.convertMoney(data, (err, data) => {
+    return ApiConnector.convertMoney(data, response => {
         if (err) {
             moneyManager.setMessage(true, err);
-        } else if (data) {
-            ProfileWidget.showProfile(data);
+        } else if (response) {
+            ProfileWidget.showProfile(response.data);
             moneyManager.setMessage(false, 'Деньги успешно конвертированы!');
         };
     });
 };
 
 moneyManager.sendMoneyCallback = data => {
-    return ApiConnector.transferMoney(data, (err, data) => {
+    return ApiConnector.transferMoney(data, response => {
         if (err) {
             moneyManager.setMessage(true, err);
-        } else if (data) {
-            ProfileWidget.showProfile(data);
+        } else if (response) {
+            ProfileWidget.showProfile(response.data);
             moneyManager.setMessage(false, 'Деньги успешно переведены!');
         };
     });
@@ -66,34 +66,34 @@ moneyManager.sendMoneyCallback = data => {
 
 const favoritesWidget = new FavoritesWidget();
 
-ApiConnector.getFavorites((err,data) => {
-    if (data) {
+ApiConnector.getFavorites(response => {
+    if (response) {
         favoritesWidget.clearTable();
-        favoritesWidget.fillTable(data);
-        favoritesWidget.updateUsersList(data);
+        favoritesWidget.fillTable(response.data);
+        favoritesWidget.updateUsersList(response.data);
     };
 });
 
 favoritesWidget.addUserCallback = data => {
-    return ApiConnector.addUserToFavorites(data, (err, data) => {
+    return ApiConnector.addUserToFavorites(data, response => {
         if (err) {
             favoritesWidget.setMessage(true, err);
-        } else if (data) {
+        } else if (response) {
             favoritesWidget.clearTable();
-            favoritesWidget.fillTable(data);
-            favoritesWidget.updateUsersList(data);
+            favoritesWidget.fillTable(response.data);
+            favoritesWidget.updateUsersList(response.data);
             favoritesWidget.setMesage(false, 'Пользователь успешно добавлен!')
         };
     });
 };
 favoritesWidget.removeUserCallback = data => {
-    return ApiConnector.removeUserFromFavorites(data, (err, data) => {
+    return ApiConnector.removeUserFromFavorites(data, response => {
         if (err) {
             moneyManager.setMessage(true, err);
-        } else if (data) {
+        } else if (response) {
             favoritesWidget.clearTable();
-            favoritesWidget.fillTable(data);
-            moneyManager.updateUsersList(data);
+            favoritesWidget.fillTable(response.data);
+            moneyManager.updateUsersList(response.data);
             favoritesWidget.setMessage(false, 'Пользователь успешно удален!')
         };
     })
